94260d0ae79f32767316d0ac4487b61eb1b2866b
Christian Stenger <cstenger@MBPdeChristian.iptv.microsoft.com>
2015-08-14 16:40:59 -0300

    Footer ok

--- footer.php
+++ footer.php
@@ -12,6 +12,7 @@
 	</div><!-- #content -->
 </div><!-- #page -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		
 		<div class="site-info row">
 			<div class="col-lg-3">
 				<ul>
@@ -45,29 +46,57 @@
 				</ul>
 			</div>
 			<div class="col-lg-3">
-				<form>
+				<form class="form-horizontal form-footer">
 					<h3>Pide tu Hora</h3>
-					
+					<div class="input-group">
+	 <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user fa-lg"></i></span>
+	 <input type="text" class="form-control" placeholder="Rut" aria-describedby="basic-addon1"> 
+	 				</div>
+	 				<div class="input-group">			
+	 <span class="input-group-addon" id="basic-addon1"><i class="fa fa-asterisk fa-lg"></i></span>
+	 <input type="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1"> 
+	 				</div>
+	 				<div class="form-group">
+	<button type="submit" class="btn btn-default enviar-btn">Ingresar</button>
+	<button type="submit" class="btn btn-default registrarse-btn">Registrarse</button>
+	 				</div>
 				</form>
+				<a class="tel-box-input" href="tel:+56225995700"><i class="fa fa-phone fa-lg"></i>+56 2 2599 57 00</a>
 			</div>
 		</div><!-- .site-info -->
-		<div class="row footer-box-2">
+		</div> <!-- cerrucho footer -->
+	</footer><!-- #colophon -->
+	<div class="bg-footer-2">
+	<div class="row footer-box-2">
 			<div class="col-lg-3">
 				<img src="<?php bloginfo('template_url'); ?>/img/acreditado.png" alt="acreditado" width="88" height="102" />
 			</div>
-			<div class="col-lg-7">
-				
+			<div class="col-lg-3">
+				<h3>Horario de Atenci&oacute;n</h3>
 			</div>
-			<div class="col-lg-2">
-				
+			<div class="col-lg-3">
+				<p>Lunes a Jueves de 09:00 a 19:00 hrs.</p>
+				<p>Viernes de 09:00 a 17:45 hrs.</p>
+				<p>Sábado de 09:30 a 13:00 hrs.</p>
+			</div>
+			<div class="col-lg-3">
+				<ul class="redes-sociales-box">
+					<li><a href="" title="Twitter COP"><i class="fa fa-twitter fa-4x"></i></a></li> 
+					<li><a href="" title="Facebook COP"><i class="fa fa-facebook fa-4x"></i></a></li>
+					<li><a href="" title="Youtube COP"><i class="fa fa-youtube fa-4x"></i></a></li>
+				</ul>
 			</div>
 		</div>
+	</div><!-- bg-footer-2 -->
+		<div class="bg-footer-3">
 		<div class="row footer-box-3">
 			<div class="col-lg-12">
-				
+				<p>Nueva de Lyon Nº 96 - Providencia - Santiago - Chile - +56 2 2599 57 00 -</p>
+				<a href="mailto:clinica@cop.cl">clinica@cop.cl</a>
+				<a href="www.cop.cl">www.cop.cl</a>
 			</div>
 		</div>	
-	</footer><!-- #colophon -->
+		</div>
 
 
 <?php wp_footer(); ?>
